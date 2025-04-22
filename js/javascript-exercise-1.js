6516878fc8aa0180a0bd7dfa0c0197404253aa04
Heinz K <heka@techcollege.dk>
2025-04-22 13:34:01 +0200

    JS Refactor modernization

--- js/javascript-exercise-1.js
+++ js/javascript-exercise-1.js
@@ -3,17 +3,17 @@
  * Tilføjer/fjerner en css klasse med større skrifttype til alle elementer
  * Benytter metoden addEventListener til at lytte på et onclick event på knappen
  */
-document.getElementById("example1").addEventListener("click", function() {
+document.getElementById("example1").addEventListener("click", () => {
     //Henter array med alle child elementer til #contentarea
     let elements = document.querySelectorAll("#contentarea *");
     //Lopper array med elementer
-    for(let i = 0; i < elements.length; i++) {
+    for(let item of elements) {
         //Tilføj klassen tx-big hvis den ikke eksisterer i et elements classList
-        if(!elements[i].classList.contains("tx-big")) {
-            elements[i].classList.add("tx-big");
+        if(!item.classList.contains("tx-big")) {
+            item.classList.add("tx-big");
         } else {
             //Ellers fjern klassen hvis den eksisterer i elementets classList
-            elements[i].classList.remove("tx-big");
+            item.classList.remove("tx-big");
         }
     }
 });
@@ -24,7 +24,7 @@ document.getElementById("example1").addEventListener("click", function() {
  * Anvender onclick metode direkte på getElementById metoden
  * Anvender forEach metode til at loope array med
  */
-document.getElementById("example2").onclick = function() {
+document.getElementById("example2").onclick = () => {
     //Henter array med alle child elementer til #contentarea og kører en foreach på arrayet
     document.querySelectorAll("#contentarea *").forEach( function(element) {
         //Toggler class tx-big til elementernes classList
